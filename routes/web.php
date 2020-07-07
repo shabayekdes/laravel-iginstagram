c6588861c7ad46cfa36c92cfb9ead8e257337e62
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-07-07 12:47:31 +0200

    Add laravel ui auth package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/instagram', 'SocialLoginController@redirectToInstagramProvider')->name('instagram.login');
+
+Route::get('login/instagram/callback', 'SocialLoginController@instagramProviderCallback')->name('instagram.login.callback');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');

08d83ebb1a2f8242ed9fd0df3ad14ffbcd22d93a
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-07-07 15:35:49 +0200

    Add access token model and migration

---
+++ app/Models/AccessToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class AccessToken extends Model
+{
+    /**
+     * Get the user that owns the phone.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ database/migrations/2020_07_07_114818_create_access_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access_tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('access_tokens');
+            $table->string('expire');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access_tokens');
+    }
+}

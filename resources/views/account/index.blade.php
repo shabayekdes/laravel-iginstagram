b1187eaea746ea8c68ea0c3af6918c82d607b82c
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-07-15 14:53:44 +0200

    Add rtmp to list accounts

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -6,9 +6,50 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                    <h5 class="card-title float-left">My Accounts</h5>
-                    <a href="#" class="btn btn-success float-right" data-toggle="modal"
-                        data-target="#staticBackdrop">Add account</a>
+                    <h5 class="card-title">My Accounts</h5>
+                </div>
+
+                <div class="card-body">
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+                    <div class="form-group row">
+                        <label for="inputEmail3" class="col-sm-2 col-form-label">RTMP:</label>
+                        <div class="col-sm-7">
+                            <input type="email" class="form-control" value="rtmps://live-upload.instagram.com:443/rtmp/"
+                                id="inputEmail3">
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="#" class="btn btn-success" data-toggle="modal" data-target="#staticBackdrop">Add
+                                account</a>
+                        </div>
+                    </div>
+                    @foreach ($tokens as $token)
+                    <div class="d-flex bd-highlight">
+                        <div class="p-2 align-self-center bd-highlight">
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            </div>
+                        </div>
+                        <div class="p-2 flex-fill bd-highlight">
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">{{ $token->username}}</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1"
+                                    aria-describedby="emailHelp">
+                            </div>
+                        </div>
+                        <div class="p-2 align-self-center bd-highlight">
+                            <button type="submit" class="btn btn-primary">Connect</button>
+                        </div>
+                    </div>
+                    @endforeach
+                    <button type="submit" class="btn btn-primary">Go Live For Selected</button>
                     <!-- Modal -->
                     <div class="modal fade" id="staticBackdrop" data-backdrop="static" tabindex="-1" role="dialog"
                         aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -32,7 +73,8 @@
                                         </div>
                                         <div class="form-group">
                                             <label for="inputPassword">Password</label>
-                                            <input type="password" name="password" class="form-control" id="inputPassword">
+                                            <input type="password" name="password" class="form-control"
+                                                id="inputPassword">
                                         </div>
                                     </div>
                                     <div class="modal-footer">
@@ -44,44 +86,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="card-body">
-                    @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
-                    <div class="form-group row">
-                        <label for="inputEmail3" class="col-sm-2 col-form-label">RTMP:</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" value="rtmps://live-upload.instagram.com:443/rtmp/" id="inputEmail3">
-                        </div>
-                    </div>
-                    @foreach ($tokens as $token)
-                    <div class="d-flex bd-highlight">
-                        <div class="p-2 align-self-center bd-highlight">
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            </div>
-                        </div>
-                        <div class="p-2 flex-fill bd-highlight">
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">{{ $token->username}}</label>
-                                <input type="email" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp">
-                            </div>
-                        </div>
-                        <div class="p-2 align-self-center bd-highlight">
-                            <button type="submit" class="btn btn-primary">Connect</button>
-                        </div>
-                    </div>
-                    @endforeach
-                    <button type="submit" class="btn btn-primary">Go Live For Selected</button>
                 </div>
             </div>
         </div>
